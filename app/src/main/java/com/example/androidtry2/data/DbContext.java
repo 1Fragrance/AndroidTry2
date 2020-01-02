89f22d99fff4404e8e5c75c1cd89861d42f9259c
OneFragrance <beekwar@gmail.com>
2020-01-03 00:05:30 +0300

    Init commit: added model, dbcontext and base layout

---
+++ app/src/main/java/com/example/androidtry2/data/DbContext.java
@@ -0,0 +1,34 @@
+package com.example.androidtry2.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.androidtry2.R;
+
+public class DbContext extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+
+    public DbContext(Context context) {
+        super(context, context.getResources().getString(R.string.db_name), null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String initializeScript = "CREATE TABLE IF NOT EXISTS products ( " +
+                                  "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                  "name TEXT, " +
+                                  "cost REAL, " +
+                                  "typeId INTEGER);";
+        db.execSQL(initializeScript);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String downScript = "DROP TABLE IF EXISTS products";
+        db.execSQL(downScript);
+        onCreate(db);
+    }
+}
